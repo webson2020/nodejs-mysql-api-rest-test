2b2cc78cca8cc87e29775723c58468addc7f31c8
webson2020 <nrossettiossa@gmail.com>
2023-08-13 09:59:02 -0400

    first commit

---
+++ db/database.sql
@@ -0,0 +1,22 @@
+
+CREATE DATABASE IF NOT EXISTS companydb;
+
+
+USE companydb
+
+CREATE TABLE employee (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name varchar(45) DEFAULT NULL,
+    salary INT(5) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
+
+
+DESCRIBE employee;
+
+
+INSERT INTO employee VALUES
+    (1, 'Joe', 1000),
+    (2, 'Henry', 2000),
+    (3, 'Sam', 2500),
+    (4, 'Max', 1500);
